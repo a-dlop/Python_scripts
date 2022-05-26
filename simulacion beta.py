8080fe124547b62dc7436b3202e0b0bcf08582d2
a-dlop <a.dlop.1729@gmail.com>
2022-05-26 13:28:46 -0500

    Add files via upload

---
+++ simulacion beta.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 26 12:00:05 2022
+
+@author: Daniel
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+def distrbeta(x,alpha,beta):
+    bet=1/scipy.special.beta(alpha,beta)*x**(alpha-1)*(1-x)**(beta-1)
+    return bet
+alpha=500
+beta=501
+variables=alpha+beta-1
+datos=list()
+from datetime import datetime
+#Método 1
+startTime = datetime.now()
+
+
+for i in range(variables):    
+    U=np.random.rand(alpha+beta-1)
+    p=np.partition(np.asarray(U), alpha)[alpha]
+    datos.append(p)
+
+plt.title("Comparación entre histograma distribucion beta metodo 1 y metodo 2" )
+plt.hist(datos,density=True)    
+
+#Python 3: 
+print(datetime.now() - startTime)
+#Método 2
+startTime = datetime.now()
+
+y1=np.random.gamma(alpha,scale=1,size=10000)
+y2=np.random.gamma(beta,scale=1,size=10000)
+Z=[y1[i]/(y2[i]+y1[i]) for i in range(len(y1))]
+plt.hist(Z,alpha=0.5,density=True)
+print(datetime.now() - startTime)
+
+import scipy
+
+x=np.linspace(0,1,num=1000)
+
+disbeta=[distrbeta(i,alpha,beta) for i in x ]
+plt.plot(x,disbeta)
+plt.show()
